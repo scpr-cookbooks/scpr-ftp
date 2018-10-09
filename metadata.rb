9c67988ac378b6f33920df40310ba79f5ad9bde3
Louise Yang <louise.m.yang@gmail.com>
2018-10-09 10:19:46 -0700

    Pins down NFS and bumps version
    * Adds in differences in chef-server

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
 name             'scpr-ftp'
-maintainer       'Eric Richardson'
-maintainer_email 'erichardson@scpr.org'
+maintainer       'Louise Yang'
+maintainer_email 'louise.yang@scpr.org'
 license          'apache2'
 description      'Configure a pure-ftpd FTP server'
 long_description 'Configure a pure-ftpd FTP server'
-version          '0.4.1'
+version          '0.4.2'
+
+depends "nfs", "= 2.4.0"
 
